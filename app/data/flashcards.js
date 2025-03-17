522a60f75720cd798a25177754395869708cb489
fedden <leonfedden@gmail.com>
2025-03-17 02:06:59 +0100

    new data

--- app/data/flashcards.js
+++ app/data/flashcards.js
@@ -37,14 +37,10 @@ So, with a heavy heart, let us begin…`
   {
     id: 4,
     title: "Competition Setup",
-    front: "Competing with me",
-    back: `The most annoying thing about being Alex's best friend is that he is always competing with you.`
-  },
-  {
-    id: 5,
-    title: "Cocktail Bar Story",
-    front: "Manager story",
-    back: `* When we were 18, I got him a job at a cocktail bar where I worked.
+    front: "Competing with me and cv",
+    back: `The most annoying thing about being Alex's best friend is that he is always competing with you.
+
+* When we were 18, I got him a job at a cocktail bar where I worked.
 * Now, I thought: "Great! My best mate is working with me. This will be fun." But three months later…
 * Pause
 * He was my manager.
@@ -64,16 +60,12 @@ If you take a look at Alex's CV, you'll notice it's written in size 72 bold font
   },
   {
     id: 7,
-    title: "Time Passing",
-    front: "Mental age joke",
+    title: "Pandemic Work",
+    front: "Mental age joke, pandemic work",
     back: `[QUICK, WITTY]
-Moving forwards, the years advance. Unfortunately, Alex's mental age didn't…`
-  },
-  {
-    id: 8,
-    title: "Covid Setup",
-    front: "Living together",
-    back: `During the Covid pandemic, Alex lived with me and my partner in London.
+Moving forwards, the years advance. Unfortunately, Alex's mental age didn't…
+
+During the Covid pandemic, Alex lived with me and my partner in London.
 
 * Naturally at the time, all work was remote, and we had this nook of the flat dedicated to office space. Nothing could go wrong.
 * So, there I was, trying to read papers, translate mathematical equations into code. And Alex was hard at work too:
@@ -84,17 +76,13 @@ I was trying to capture the tranquillity of an office, and Alex thought he was s
   },
   {
     id: 9,
-    title: "Change Coming",
-    front: "Something changed",
+    title: "Change Coming: Footsteps Story",
+    front: "Something changed: New footsteps",
     back: `But towards the tail end of the pandemic, when things started to open up, something changed.
 
-The thing about living with Alex, is that he tends to walk quite flat footed:`
-  },
-  {
-    id: 10,
-    title: "Footsteps Story",
-    front: "New footsteps",
-    back: `At 2 AM, I was used to hearing the sound: 'DOOF. DOOF. DOOF.'
+The thing about living with Alex, is that he tends to walk quite flat footed:
+
+At 2 AM, I was used to hearing the sound: 'DOOF. DOOF. DOOF.'
 Alex, the hunter-gatherer, returns from another night out with a chicken wrap in hand.
 But then… a new sound: 'tap tap tap.' It was delicate. It was graceful… It was quite stealthy, but it was… the unmistakable sound of a second set of footsteps.
 
@@ -103,23 +91,21 @@ Alex had tricked someone into liking him.`
   },
   {
     id: 11,
-    title: "Alexandra Intro",
-    front: "Not just anyone",
+    title: "Alexandra Qualities",
+    front: "Not just anyone, getting to know her, building Alex up",
     back: `And not just anyone.
 A member of the opposite sex.
 And not just any member of the opposite sex…
-Someone as bestowed with qualities as Alexandra.`
-  },
-  {
-    id: 12,
-    title: "Alexandra Qualities",
-    front: "Getting to know her",
-    back: `* Over time, I have had the chance to get to know her properly.
+Someone as bestowed with qualities as Alexandra.
+
+* Over time, I have had the chance to get to know her properly.
 * She is intelligent, kind, and endlessly patient.
 * Pause
 * I mean, she did agree to marry Alex after all!
 
-We are all here because we loved Alex before he met Alexandra, but undeniably, she has built him up for the better as well. Nobel Prizes have been given out for less.`
+We are all here because we loved Alex before he met Alexandra, but undeniably, she has built him up for the better as well. 
+
+Nobel Prizes have been given out for less.`
   },
   {
     id: 13,
@@ -134,29 +120,27 @@ We are all here because we loved Alex before he met Alexandra, but undeniably, s
   {
     id: 14,
     title: "Being Crap",
-    front: "Both being crap",
+    front: "Both being crap, Alex's nature",
     back: `So, if I've ripped into Alex for being a bit crap, I would like to remind you all of my remit in this speech: Telling the truth.
 
 And, this truth extends further.
 
 I should highlight that I am also incredibly crap, and the crucial thing is, Alex has always suffered me.
 
-To abbreviate decades, I've wasted his time, patience, money – and much else - yet to Alex's credit, he's always relaxed and is ready to go again.`
-  },
-  {
-    id: 15,
-    title: "Alex's Nature",
-    front: "Force of nature",
-    back: `Alex is a true force of nature. Nothing gets him down - unless arsenal loses - and he's always been so confident and collected with everyone we meet.`
+To abbreviate decades, I've wasted his time, patience, money – and much else - yet to Alex's credit, he's always relaxed and is ready to go again.
+
+Alex is a true force of nature. Nothing gets him down - unless arsenal loses - and he's always been so confident and collected with everyone we meet.`
   },
   {
     id: 16,
     title: "Final Truth",
-    front: "Separate paths",
+    front: "Separate paths, tie back to joint steps",
     back: `[EMPHASIS ON TRUTH NOW]
 The truth is, me and Alex were really tight growing up, and my feeling is that over time life has taken us down separate paths.
 
-And that's okay for me, because it's clear how brilliantly Alex and Alexandra have been in creating a life filled with adventure and happiness. Those joint steps that I heard at 2AM all those years ago have led them here today. And I am sure they will lead them onwards to a fulfilling and joyful married life.`
+And that's okay for me, because it's clear how brilliantly Alex and Alexandra have been in creating a life filled with adventure and happiness. 
+
+Those joint steps that I heard at 2AM all those years ago have led them here today. And I am sure they will lead them onwards to a fulfilling and joyful married life.`
   },
   {
     id: 17,
